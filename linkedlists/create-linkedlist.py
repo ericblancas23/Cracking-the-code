eb053d9750fe8c900a8d9213277c254e1c844ef5
EricB <ericblancas23@gmail.com>
2019-05-16 00:21:08 -0700

    linkedlist

---
+++ linkedlists/create-linkedlist.py
@@ -0,0 +1,30 @@
+class Node(object):
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
+    def get_data(self):
+        return self.data
+    def get_next(self):
+        return self.get_next
+    def new_next(self, new_next):
+        self.next_node = new_next
+
+class linkedlist(data):
+    def __init__(self):
+        self.head = head
+        self.tail = tail
+    
+    def insert(self, data):
+        new_node = Node(data)
+        new_node.set_next(self.head)
+        self.head = new_node
+    
+    def set_head(self, data):
+        pass
+    
+    def set_tail(self, data):
+        pass
+
+    def remove (self, data):
+        pass
+
